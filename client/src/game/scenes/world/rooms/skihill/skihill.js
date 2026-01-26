e868d0a11190b203a253bd7f3643fbda177aa996
x <pacmooooon123a@outlook.com>
2026-01-26 14:52:18 +0000

    Improved Ski Hill Room Scene

--- client/src/game/scenes/world/rooms/skihill/skihill.js
+++ client/src/game/scenes/world/rooms/skihill/skihill.js
@@ -6,7 +6,7 @@ import { BaseScene } from "../../../base/baseScene";
 
 export class SkihillScene extends BaseScene {
     constructor() {
-        super("SkihillScene");
+        super("SkiHillScene");
     }
 
     init(data) {
@@ -31,73 +31,78 @@ export class SkihillScene extends BaseScene {
     }
 
     createContent() {
-		// skihill_sky_png
-		const skihill_sky_png = this.add.image(640, 58, "skihill", "skihill_sky.png");
-		skihill_sky_png.scaleX = 0.7527151893627061;
+				// skihill_sky_png
+		const skihill_sky_png = this.add.image(711, 139, "skihill", "skihill_sky.png");
+		skihill_sky_png.scaleX = 0.9955363415032087;
+		skihill_sky_png.scaleY = 1.1620353373740027;
 
 		// skihill_background_png
-		const skihill_background_png = this.add.image(661, 427, "skihill", "skihill_background.png");
-		skihill_background_png.scaleX = 0.8612321223839137;
-		skihill_background_png.scaleY = 0.8656567001835681;
+		const skihill_background_png = this.add.image(-175, 119, "skihill", "skihill_background.png");
+		skihill_background_png.scaleX = 0.9752745429207709;
+		skihill_background_png.scaleY = 1.1241444579320303;
+		skihill_background_png.setOrigin(0, 0);
+
+		// skihill_line_two_png
+		const skihill_line_two_png = this.add.image(937, 98, "skihill", "skihill_line_two.png");
+		skihill_line_two_png.scaleX = 0.8657394748027144;
+		skihill_line_two_png.scaleY = 0.9414447230969358;
 
 		// ski_hill_queue_png
-		const ski_hill_queue_png = this.add.image(1139, 358, "skihill", "ski_hill_queue.png");
-		ski_hill_queue_png.scaleX = 1.3597858873896147;
-		ski_hill_queue_png.scaleY = 1.0043278721100586;
+		const ski_hill_queue_png = this.add.image(1127, 360, "skihill", "ski_hill_queue.png");
+		ski_hill_queue_png.scaleX = 1.0074168964558892;
+		ski_hill_queue_png.scaleY = 1.0250146731755228;
 
 		// skihill_line_png_1
 		const skihill_line_png_1 = this.add.image(1075, 181, "skihill", "skihill_line.png");
 		skihill_line_png_1.scaleX = 0.7745135539894845;
 		skihill_line_png_1.scaleY = 0.5934581978882644;
 		skihill_line_png_1.angle = 13;
+		skihill_line_png_1.visible = false;
 
 		// ski_hill_main_png
-		const ski_hill_main_png = this.add.image(665, 439, "skihill", "ski_hill_main.png");
-		ski_hill_main_png.scaleX = 0.8360181914732332;
-		ski_hill_main_png.scaleY = 0.8485808693907207;
+		const ski_hill_main_png = this.add.image(727, 558, "skihill", "ski_hill_main.png");
+		ski_hill_main_png.scaleX = 0.928777877299569;
+		ski_hill_main_png.scaleY = 1.002693317923844;
 
 		// skihill_shack_behind_png
-		this.add.image(906, 214, "skihill", "skihill_shack_behind.png");
-
-		// skihill_line_two_png
-		const skihill_line_two_png = this.add.image(826, 68, "skihill", "skihill_line_two.png");
-		skihill_line_two_png.scaleX = 0.8657394748027144;
-		skihill_line_two_png.scaleY = 0.9414447230969358;
+		const skihill_shack_behind_png = this.add.image(975, 256, "skihill", "skihill_shack_behind.png");
+		skihill_shack_behind_png.scaleX = 1.3182027428683138;
+		skihill_shack_behind_png.scaleY = 1.1584834604689804;
 
 		// skihill_line_png
-		const skihill_line_png = this.add.image(792, 99, "skihill", "skihill_line.png");
+		const skihill_line_png = this.add.image(895, 131, "skihill", "skihill_line.png");
 		skihill_line_png.scaleX = 0.7421606576365563;
 		skihill_line_png.scaleY = 0.8363259454246562;
 
 		// skihill_shack_png
-		const skihill_shack_png = this.add.image(942, 179, "skihill", "skihill_shack.png");
-		skihill_shack_png.scaleX = 0.7431381503849253;
-		skihill_shack_png.scaleY = 0.7040297945418124;
+		const skihill_shack_png = this.add.image(1033, 207, "skihill", "skihill_shack.png");
+		skihill_shack_png.scaleX = 1.1515277188926207;
+		skihill_shack_png.scaleY = 0.9641788909843253;
 
 		// skihill_top_part_png
-		const skihill_top_part_png = this.add.image(522, 161, "skihill", "skihill_top_part.png");
-		skihill_top_part_png.scaleX = 0.7561052224681684;
-		skihill_top_part_png.scaleY = 0.8113683823521645;
+		const skihill_top_part_png = this.add.image(574, 214, "skihill", "skihill_top_part.png");
+		skihill_top_part_png.scaleX = 0.9391392757482948;
+		skihill_top_part_png.scaleY = 0.924923754215114;
 
 		// skihill_green_paper0001_png
-		const skihill_green_paper0001_png = this.add.image(429, 133, "skihill", "skihill_green_paper0001.png");
-		skihill_green_paper0001_png.scaleX = 0.5681750844040137;
-		skihill_green_paper0001_png.scaleY = 0.6106997703811348;
+		const skihill_green_paper0001_png = this.add.image(456, 181, "skihill", "skihill_green_paper0001.png");
+		skihill_green_paper0001_png.scaleX = 0.6345773750833913;
+		skihill_green_paper0001_png.scaleY = 0.8472977713749209;
 
 		// skihill_green_arrow_png
-		const skihill_green_arrow_png = this.add.image(855, 478, "skihill", "skihill_green_arrow.png");
-		skihill_green_arrow_png.scaleX = 0.5547107830015992;
-		skihill_green_arrow_png.scaleY = 0.5933581665407577;
+		const skihill_green_arrow_png = this.add.image(944, 648, "skihill", "skihill_green_arrow.png");
+		skihill_green_arrow_png.scaleX = 0.863641354695569;
+		skihill_green_arrow_png.scaleY = 0.9002086603203987;
 
 		// skihill_green_arrow_snow_png
-		const skihill_green_arrow_snow_png = this.add.image(1037, 436, "skihill", "skihill_green_arrow_snow.png");
-		skihill_green_arrow_snow_png.scaleX = 0.7412449375057589;
-		skihill_green_arrow_snow_png.scaleY = 0.8463264763521018;
+		const skihill_green_arrow_snow_png = this.add.image(1138, 544, "skihill", "skihill_green_arrow_snow.png");
+		skihill_green_arrow_snow_png.scaleX = 0.9708151311967932;
+		skihill_green_arrow_snow_png.scaleY = 1.044165333757181;
 
 		// skihill_blue_arrow_snow_png
-		const skihill_blue_arrow_snow_png = this.add.image(422, 477, "skihill", "skihill_blue_arrow_snow.png");
-		skihill_blue_arrow_snow_png.scaleX = 0.8124531843274214;
-		skihill_blue_arrow_snow_png.scaleY = 0.8078284099990185;
+		const skihill_blue_arrow_snow_png = this.add.image(464, 585, "skihill", "skihill_blue_arrow_snow.png");
+		skihill_blue_arrow_snow_png.scaleX = 0.9171864584957394;
+		skihill_blue_arrow_snow_png.scaleY = 0.9802705469423592;
 
 		this.audioManager.play(SKI_HILL_ROOM_MUSIC);
     }
