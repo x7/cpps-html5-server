d5c898f8232895c59ee34c0f3f16af04d05b5b6d
x <pacmooooon123a@outlook.com>
2026-01-26 15:10:44 +0000

    Improved Gift Shop Room Scene

--- client/src/game/scenes/world/rooms/giftshop/giftshop.js
+++ client/src/game/scenes/world/rooms/giftshop/giftshop.js
@@ -5,7 +5,7 @@ import { BaseScene } from "../../../base/baseScene";
 
 export class GiftshopScene extends BaseScene {
     constructor() {
-        super("GiftshopScene");
+        super("GiftShopScene");
     }
 
     init(data) {
@@ -30,88 +30,90 @@ export class GiftshopScene extends BaseScene {
     }
 
     createContent() {
-        // giftshop_main_png
-		const giftshop_main_png = this.add.image(639, 348, "giftshop", "giftshop_main.png");
-		giftshop_main_png.scaleX = 0.8341172842105191;
-		giftshop_main_png.scaleY = 0.7989655305329657;
-
-		// giftshop_chair_one_png
-		const giftshop_chair_one_png = this.add.image(897, 472, "giftshop", "giftshop_chair_one.png");
-		giftshop_chair_one_png.scaleX = 0.8063047279890161;
-		giftshop_chair_one_png.scaleY = 0.8427180897400216;
-
-		// giftshop_chair_two_png
-		const giftshop_chair_two_png = this.add.image(850, 537, "giftshop", "giftshop_chair_two.png");
-		giftshop_chair_two_png.scaleX = 0.9073095875486503;
-		giftshop_chair_two_png.scaleY = 0.9027477597206355;
-
-		// giftshop_chair_three_png
-		const giftshop_chair_three_png = this.add.image(1130, 260, "giftshop", "giftshop_chair_three.png");
-		giftshop_chair_three_png.scaleX = 0.7525579679235725;
-		giftshop_chair_three_png.scaleY = 0.679815267517664;
-
-		// giftshop_counter_png
-		this.add.image(1124, 345, "giftshop", "giftshop_counter.png");
-
-		// giftshop_watches0001_png
-		const giftshop_watches0001_png = this.add.sprite(1177, 320, "giftshop", "giftshop_watches0001.png");
-		giftshop_watches0001_png.scaleX = 0.42545940775314095;
-		giftshop_watches0001_png.scaleY = 0.40176609180343753;
-
-		// giftshop_cash_register0001_png
-		const giftshop_cash_register0001_png = this.add.sprite(1053, 254, "giftshop", "giftshop_cash_register0001.png");
-		giftshop_cash_register0001_png.scaleX = 0.8954750343146596;
-		giftshop_cash_register0001_png.scaleY = 0.7823409037511551;
-
-		// giftshop_speakers0001_png
-		const giftshop_speakers0001_png = this.add.sprite(636, 152, "giftshop", "giftshop_speakers0001.png");
-		giftshop_speakers0001_png.scaleX = 0.8382762739375111;
-		giftshop_speakers0001_png.scaleY = 0.7967244747027603;
-
-		// giftshop_curtain0001_png
-		const giftshop_curtain0001_png = this.add.sprite(385, 195, "giftshop", "giftshop_curtain0001.png");
-		giftshop_curtain0001_png.scaleX = 0.8623817951869084;
-		giftshop_curtain0001_png.scaleY = 0.7227630932941281;
-
-		// giftshop_clothes_png
-		const giftshop_clothes_png = this.add.image(302, 354, "giftshop", "giftshop_clothes.png");
-		giftshop_clothes_png.scaleX = 0.8834237598933798;
-		giftshop_clothes_png.scaleY = 0.8962818601092272;
-
-		// giftshop_door0001_png
-		const giftshop_door0001_png = this.add.image(923, 164, "giftshop", "giftshop_door0001.png");
-		giftshop_door0001_png.scaleX = 0.8639646815147847;
-		giftshop_door0001_png.scaleY = 0.8101392975615017;
-
-		// giftshop_chair_four_png
-		const giftshop_chair_four_png = this.add.image(250, 481, "giftshop", "giftshop_chair_four.png");
-		giftshop_chair_four_png.scaleX = 0.8277605975483534;
-		giftshop_chair_four_png.scaleY = 0.7770450616314644;
-
-		// giftshop_chair_four_arm_png_1
-		const giftshop_chair_four_arm_png_1 = this.add.image(264, 541, "giftshop", "giftshop_chair_four_arm.png");
-		giftshop_chair_four_arm_png_1.scaleX = 0.8821788296805595;
-		giftshop_chair_four_arm_png_1.scaleY = 0.8653492940459486;
-
-		// giftshop_chair_four_png_1
-		const giftshop_chair_four_png_1 = this.add.image(286, 582, "giftshop", "giftshop_chair_four.png");
-		giftshop_chair_four_png_1.scaleX = 0.8277605975483534;
-		giftshop_chair_four_png_1.scaleY = 0.7770450616314644;
-
-		// giftshop_chair_four_arm_png
-		const giftshop_chair_four_arm_png = this.add.image(291, 632, "giftshop", "giftshop_chair_four_arm.png");
-		giftshop_chair_four_arm_png.scaleX = 0.8821788296805595;
-		giftshop_chair_four_arm_png.scaleY = 0.8653492940459486;
+		// giftshop_main_png
+		const giftshop_main_png = this.add.image(-117, -41, "giftshop", "giftshop_main.png");
+		giftshop_main_png.scaleX = 0.9958943252972757;
+		giftshop_main_png.setOrigin(0, 0);
 
 		// giftshop_bottom_fish_png
-		const giftshop_bottom_fish_png = this.add.image(622, 359, "giftshop", "giftshop_bottom_fish.png");
-		giftshop_bottom_fish_png.scaleX = 0.8001464119979486;
-		giftshop_bottom_fish_png.scaleY = 0.8394006326069317;
+		const giftshop_bottom_fish_png = this.add.image(733, 495, "giftshop", "giftshop_bottom_fish.png");
+		giftshop_bottom_fish_png.scaleX = 1.0072285539494639;
+		giftshop_bottom_fish_png.scaleY = 1.0158509427060216;
 
-        // giftshop_door0002_png
-		const giftshop_door0002_png = this.add.image(922, 165, "giftshop", "giftshop_door0002.png");
-		giftshop_door0002_png.scaleX = 0.8936018970163114;
-		giftshop_door0002_png.scaleY = 0.8113039455908615;
+		// giftshop_chair_one_png
+		const giftshop_chair_one_png = this.add.image(1095, 626, "giftshop", "giftshop_chair_one.png");
+		giftshop_chair_one_png.scaleX = 0.7730058883734877;
+		giftshop_chair_one_png.scaleY = 0.765819493532557;
+
+		// giftshop_chair_two_png
+		const giftshop_chair_two_png = this.add.image(1027, 703, "giftshop", "giftshop_chair_two.png");
+		giftshop_chair_two_png.scaleX = 0.8828193203111082;
+		giftshop_chair_two_png.scaleY = 0.9958005547647024;
+
+		// giftshop_chair_three_png
+		const giftshop_chair_three_png = this.add.image(1366, 384, "giftshop", "giftshop_chair_three.png");
+		giftshop_chair_three_png.scaleX = 1.06280078513135;
+		giftshop_chair_three_png.scaleY = 0.8431859648966638;
+
+		// giftshop_counter_png
+		const giftshop_counter_png = this.add.image(1347, 466, "giftshop", "giftshop_counter.png");
+		giftshop_counter_png.scaleX = 1.1497372224116273;
+		giftshop_counter_png.scaleY = 1.2252148278035895;
+
+		// giftshop_watches0001_png
+		const giftshop_watches0001_png = this.add.sprite(1407, 442, "giftshop", "giftshop_watches0001.png");
+		giftshop_watches0001_png.scaleX = 0.46428219009474814;
+		giftshop_watches0001_png.scaleY = 0.45112374089539253;
+
+		// giftshop_cash_register0001_png
+		const giftshop_cash_register0001_png = this.add.sprite(1276, 364, "giftshop", "giftshop_cash_register0001.png");
+		giftshop_cash_register0001_png.scaleX = 0.8720126380575843;
+		giftshop_cash_register0001_png.scaleY = 0.7910136529145547;
+
+		// giftshop_speakers0001_png
+		const giftshop_speakers0001_png = this.add.sprite(747, 228, "giftshop", "giftshop_speakers0001.png");
+		giftshop_speakers0001_png.scaleX = 0.986864014002472;
+		giftshop_speakers0001_png.scaleY = 1.0275086975098104;
+
+		// giftshop_curtain0001_png
+		const giftshop_curtain0001_png = this.add.sprite(446, 282, "giftshop", "giftshop_curtain0001.png");
+		giftshop_curtain0001_png.scaleX = 0.8623817951869084;
+		giftshop_curtain0001_png.scaleY = 0.9586460794322212;
+
+		// giftshop_clothes_png
+		const giftshop_clothes_png = this.add.image(350, 475, "giftshop", "giftshop_clothes.png");
+		giftshop_clothes_png.scaleX = 1.0601878935022935;
+		giftshop_clothes_png.scaleY = 1.031001967903876;
+
+		// giftshop_door0001_png
+		const giftshop_door0001_png = this.add.image(1091, 240, "giftshop", "giftshop_door0001.png");
+		giftshop_door0001_png.scaleX = 1.0367026714260419;
+		giftshop_door0001_png.scaleY = 0.984027915938957;
+
+		// giftshop_chair_four_png
+		const giftshop_chair_four_png = this.add.image(318, 780, "giftshop", "giftshop_chair_four.png");
+		giftshop_chair_four_png.scaleX = 0.9241198991873245;
+		giftshop_chair_four_png.scaleY = 0.9115332169037532;
+
+		// giftshop_chair_four_arm_png_1
+		const giftshop_chair_four_arm_png_1 = this.add.image(322, 840, "giftshop", "giftshop_chair_four_arm.png");
+		giftshop_chair_four_arm_png_1.scaleX = 0.8217715947129681;
+		giftshop_chair_four_arm_png_1.scaleY = 0.8266244164140131;
+
+		// giftshop_chair_four_png_1
+		const giftshop_chair_four_png_1 = this.add.image(284, 645, "giftshop", "giftshop_chair_four.png");
+		giftshop_chair_four_png_1.scaleX = 0.9241198991873245;
+		giftshop_chair_four_png_1.scaleY = 0.9115332169037532;
+
+		// giftshop_chair_four_arm_png
+		const giftshop_chair_four_arm_png = this.add.image(286, 707, "giftshop", "giftshop_chair_four_arm.png");
+		giftshop_chair_four_arm_png.scaleX = 0.8217715947129681;
+		giftshop_chair_four_arm_png.scaleY = 0.8266244164140131;
+
+		// giftshop_door0002_png
+		const giftshop_door0002_png = this.add.image(1091, 240, "giftshop", "giftshop_door0002.png");
+		giftshop_door0002_png.scaleX = 1.0367026714260419;
+		giftshop_door0002_png.scaleY = 0.984027915938957;
 		giftshop_door0002_png.visible = false;
 
         // Animations start here
